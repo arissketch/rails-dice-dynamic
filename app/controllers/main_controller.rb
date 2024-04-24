4d3b1d5e8aaffc71640c13a4b8749973586adf19
arissketch <60331491+arissketch@users.noreply.github.com>
2024-04-24 17:15:34 +0000

    one

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,18 @@
+class MainController < ApplicationController
+  def home
+    render({:template=>"layouts/homepage"})
+  end
+
+  def d2_6page
+    render({:template=>"layouts/homepage"})
+  end
+  def d2_10page
+    render({:template=>"layouts/homepage"})
+  end
+  def d1_20page
+    render({:template=>"layouts/homepage"})
+  end
+  def d5_4page
+    render({:template=>"layouts/homepage"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get("/",{:controller=>"main", :action=> "home"})
+  get("/2d6",{:controller=>"main", :action=> "d2_6page"})
+  get("/2d10",{:controller=>"main", :action=> "d2_10page"})
+  get("/1d20",{:controller=>"main", :action=> "d1_20page"})
+  get("/5d4",{:controller=>"main", :action=> "d5_4page"})
 
 end
