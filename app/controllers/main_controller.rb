1e653b96288e08a8b2bddedabef61a5a36670977
arissketch <60331491+arissketch@users.noreply.github.com>
2024-04-25 00:06:22 +0000

    final

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,15 +4,55 @@ class MainController < ApplicationController
   end
 
   def d2_6page
-    render({:template=>"layouts/homepage"})
+    @no_dice="2"
+    @no_side="6"
+    @result=[]
+    2.times do
+      @side_result=rand(1..6)
+      @result.push(@side_result)
+    end
+    render({:template=>"layouts/dice"})
   end
   def d2_10page
-    render({:template=>"layouts/homepage"})
+    @no_dice="2"
+    @no_side="10"
+    @result=[]
+    2.times do
+      @side_result=rand(1..10)
+      @result.push(@side_result)
+    end
+    render({:template=>"layouts/dice"})
   end
   def d1_20page
-    render({:template=>"layouts/homepage"})
+    @no_dice="1"
+    @no_side="20"
+    @result=[]
+    1.times do
+      @side_result=rand(1..20)
+      @result.push(@side_result)
+    end
+    render({:template=>"layouts/dice"})
   end
   def d5_4page
-    render({:template=>"layouts/homepage"})
+    @no_dice="5"
+    @no_side="4"
+    @result=[]
+    5.times do
+      @side_result=rand(1..4)
+      @result.push(@side_result)
+    end
+    render({:template=>"layouts/dice"})
   end
+
+  def random
+    @no_dice=params.fetch("random_dice").to_i
+    @no_side=params.fetch("random_sides").to_i
+    @result=[]
+    @no_dice.times do
+      @side_result=rand(1..@no_side)
+      @result.push(@side_result)
+    end
+    render({:template => "layouts/dice"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get("/",{:controller=>"main", :action=> "home"})
-  get("/2d6",{:controller=>"main", :action=> "d2_6page"})
-  get("/2d10",{:controller=>"main", :action=> "d2_10page"})
-  get("/1d20",{:controller=>"main", :action=> "d1_20page"})
-  get("/5d4",{:controller=>"main", :action=> "d5_4page"})
+  get("/dice/2/6",{:controller=>"main", :action=> "d2_6page"})
+  get("/dice/2/10",{:controller=>"main", :action=> "d2_10page"})
+  get("/dice/1/20",{:controller=>"main", :action=> "d1_20page"})
+  get("/dice/5/4",{:controller=>"main", :action=> "d5_4page"})
+  get("/dice/:random_dice/:random_sides",{:controller=>"main", :action=> "random"})
+
 
 end
